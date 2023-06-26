5d5d29925dddd49f3b1f3c3e257c56c234c50cf1
Fernando Gomez <dim.dei@gmail.com>
2023-06-26 18:22:56 -0500

    Actualizacion jun26/06-18:22

--- Landing page/script.js
+++ Landing page/script.js
@@ -1,12 +1,6 @@
 
 //=========comienzo Menú hamburguesa=========
-const elment = document.querySelector('.hamburger');
-const elment1 = document.querySelector('.nav-list');
 
-elment.addEventListener("click", () => {
-  elment.classList.toggle('active');
-  elment1.classList.toggle('active');
-});
 //========fin menú hamburguesa===============
 
 
